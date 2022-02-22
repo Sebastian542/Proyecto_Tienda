7f76720485ea028fa522d14a8629c3176c541f6f
omsaalejo <osantos@unbosque.edu.co>
2022-02-21 20:51:16 -0500

    Subir Metodo 3

--- src/main/java/co/edu/unbosque/model/persistence/Persistencia.java
+++ src/main/java/co/edu/unbosque/model/persistence/Persistencia.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.nio.charset.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.stream.Collectors;
 public class Persistencia {
 	
 	public Persistencia() {}
@@ -34,5 +35,65 @@ public class Persistencia {
 		} catch(Exception e) { e.printStackTrace(); }
 		return list;
 	}
+	public static int countByStockCode(String stockCode) {
+		int sum = 0;
+		try (BufferedReader reader = Files.newBufferedReader(Paths.get("src/main/resources/Archives/data.csv"), Charset.defaultCharset())){
+			String content = reader.readLine();
+			while(reader.ready()) {
+				content = reader.readLine();
+				String line [] = content.split(",");
+				if(stockCode.equalsIgnoreCase(String.valueOf(line[1]))) 
+					sum += Integer.parseInt(line[3]);
+			}
+		} catch(Exception e) { e.printStackTrace(); }
+		return sum;
+	}
+	
+	
+	public static String avgMonthlySales(boolean groupByCountry) {
+		HashMap <String, Double> averageCountry = new HashMap <> ();
+		HashMap <String, Integer> countCountrys = new HashMap <> ();
+		try (BufferedReader reader = Files.newBufferedReader(Paths.get("src/main/resources/Archives/data.csv"), Charset.defaultCharset())){
+			String content = reader.readLine();
+			while(reader.ready()) {
+				content = reader.readLine();
+				String line [] = content.split(",");
+				String country = line[line.length -1];
+				if(groupByCountry) {
+					double sum = Double.parseDouble(line[3]) * Double.parseDouble(line[5]);
+					if(!averageCountry.containsKey(country)) {
+						countCountrys.put(country, 1);
+						averageCountry.put(country, sum);
+					}else {
+						double current = averageCountry.get(country);
+						averageCountry.replace(country, (current + sum));
+						countCountrys.replace(country, countCountrys.get(country) +1);
+					}
+					for(String index : averageCountry.keySet()) 
+						averageCountry.replace(index, averageCountry.get(index) / countCountrys.get(index));
+					return averageCountry.entrySet().stream().map(x -> String.valueOf(x)).collect(Collectors.joining("\n"));
+				}else {
+					HashMap <Integer, Double> month = new HashMap <Integer, Double> ();
+					HashMap <Integer, Integer> quantity = new HashMap <Integer, Integer> ();
+					String split [] = line[4].split("/");
+					int mes = Integer.parseInt(split[2]);
+					if(!month.containsKey(Integer.parseInt(line[4]))) {
+						month.put(mes, Integer.parseInt(line[3]) * Double.parseDouble(line[5]));
+						quantity.put(mes, 1);
+					}else {
+						double current = month.get(mes);
+						month.replace(mes, (current + (Integer.parseInt(line[3]) * Double.parseDouble(line[5]))));
+						quantity.replace(mes, quantity.get(mes));
+					}
+					for(Integer index : month.keySet())
+						month.replace(index, (month.get(index) / quantity.get(index)));
+					return month.entrySet().stream().map(x -> String.valueOf(x)).collect(Collectors.joining("\n"));
+				}
+			}
+		} catch(Exception e) { e.printStackTrace(); }
+		return null;
+	}
+	
+	
 	
 }
