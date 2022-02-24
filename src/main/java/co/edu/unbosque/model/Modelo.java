3ac52f372e83b487cae9eed2e34ffdbc9bc3583b
NicolasCamachoL <84592597+NicolasCamachoL@users.noreply.github.com>
2022-02-23 21:53:14 -0500

    Añadido instancia de los metodos 2 al 6

--- src/main/java/co/edu/unbosque/model/Modelo.java
+++ src/main/java/co/edu/unbosque/model/Modelo.java
@@ -2,11 +2,37 @@ package co.edu.unbosque.model;
 
 public class Modelo {
 
-	public Modelo() {
-		
+	public Modelo() {		
 		
 	}
 	
+	public void summaraizingSales() {
+		System.out.println(Persistencia.sumTotalSales());
+	}
+
+	public void searchData(String search) {
+		System.out.println(Persistencia.findByInvoiceNo(search));
+	}
+
+	public void countByStockCode(String code) {
+		System.out.println(Persistencia.countByStockCode(code));
+	}
+
+	public void avgMonthlySales(boolean confirmar) {
+		System.out.println(Persistencia.avgMonthlySales(confirmar));
+	}
+
+	public void findPartiallyByDescription(String search) {
+		Persistencia.findByInvoiceNo(search).forEach(x -> System.out.println(x));
+	}
+
+	public void findPartiallyByDescriptionOrder(String search, boolean order) {
+		Persistencia.findPartiallyByDescription(search, order).forEach(x -> System.out.println(x));
+	}
+
+	public void findPartiallyByDescriptionFilter(String search, int initMonth, int endMonth) {
+		System.out.println(Persistencia.finPartiallyByDescription(search, initMonth, endMonth));
+	}			
 	
 	
 	
